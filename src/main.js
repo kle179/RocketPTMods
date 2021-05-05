6547bae0b7c83afe271411c1b270dd7e2678844d
kle179 <82794305+kle179@users.noreply.github.com>
2021-05-05 12:24:57 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-//Kylien Le
+//Kylien Le, Rocket PatrolMods 4/28/2021/ It took me about 9-10 hours to figure out the project
+//I've implemented a background music to Play Scene
+//I allowed the player to control the rocket after it being fired.
+//I created a new animated sprite for the SpaceShip enemies
+//I changed the game's artwork making a stream of water with fish and a bobber to create a theme about fishing.
+//I changed the sfx
+//And I implemented a two player mode.
 let config = {
     type: Phaser.CANVAS,
     width: 640,
